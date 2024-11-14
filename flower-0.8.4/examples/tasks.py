8b3ad57e8b7997f811b5df55944410cea38d51d4
seeunsama <ase021125@gmail.com>
2024-11-14 16:51:04 +0900

    update

---
+++ flower-0.8.4/examples/tasks.py
@@ -0,0 +1,32 @@
+import time
+from datetime import datetime
+
+from celery import Celery
+
+
+app = Celery("tasks", broker="amqp://", backend="amqp")
+app.conf.CELERY_ACCEPT_CONTENT = ['pickle', 'json', 'msgpack', 'yaml']
+
+
+@app.task
+def add(x, y):
+    return x + y
+
+
+@app.task
+def sleep(seconds):
+    time.sleep(seconds)
+
+
+@app.task
+def echo(msg, timestamp=False):
+    return "%s: %s" % (datetime.now(), msg) if timestamp else msg
+
+
+@app.task
+def error(msg):
+    raise Exception(msg)
+
+
+if __name__ == "__main__":
+    app.start()
